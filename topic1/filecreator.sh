983b1ecca4c752b156dfb5f2c13fb6f64fc94a29
filipo18 <54606994+filipo18@users.noreply.github.com>
2019-09-18 09:23:35 +0900

    Add files via upload

---
+++ topic1/filecreator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This program creates files inside a folder
+
+echo "Create the folder"
+mkdir test
+
+#entering the folder
+cd test
+
+#creating 100 files
+for (( f=0; f<100; f++ ))
+do
+    echo "Creating file $f"
+    echo "Message #$f" > file_$f.txt
+done
+
