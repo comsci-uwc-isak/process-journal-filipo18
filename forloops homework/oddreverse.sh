0fe3270c9355d6ea040e1ac1551377d278ac8ffc
filipo18 <54606994+filipo18@users.noreply.github.com>
2019-10-16 19:17:41 +0900

    Delete oddreverse.sh

--- forloops homework/oddreverse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#This program will print all the odd numbers from 1001 to 1 reverse
-
-#If I start with 1001 and decuce 2 in every next repetition I will get just odd numbers
-for (( i = 1001; i > 0; i-=2 )); do
-    echo $i
-done
