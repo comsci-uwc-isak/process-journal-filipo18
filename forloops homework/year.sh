94702b7326320ad48931494ce36e49882352a75d
filipo18 <54606994+filipo18@users.noreply.github.com>
2019-10-16 19:19:02 +0900

    Delete year.sh

--- forloops homework/year.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#this program will print out years from 1000 to 2019 with word year attached
-
-#for loop will reapeat itself and always increase i by 1 1019 times
-
-for (( i = 1000; i < 2020; i++ )); do
-  echo "Year:" $i
-done
