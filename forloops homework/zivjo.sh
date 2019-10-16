bf0affb58a471668ac3f0f5bfa6890329e142476
filipo18 <54606994+filipo18@users.noreply.github.com>
2019-10-16 19:18:54 +0900

    Delete zivjo.sh

--- forloops homework/zivjo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#this program will print greeting in my language "zivjo" 1000 times
-
-#This for loop will increase i from 0 to 1000 by 1
-for (( i = 0; i < 1001; i++ )); do
-    echo "Živjo!"
-done
